aa3995ebd539e1ad5152e8cbc5d6416d409d903a
d <dmitriypospelov93@gmail.com>
2024-05-17 12:13:35 +0700

    FIRST COMMIT

---
+++ src/main/java/com/effectivemobile/bankoperationsservice/BankOperationsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.effectivemobile.bankoperationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOperationsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankOperationsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/effectivemobile/bankoperationsservice/BankOperationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.effectivemobile.bankoperationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOperationsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
